ee55fa6f747e90aef2149761bdadf8c7dea9a929
Victor Malov <victor.malov@breeffy.com>
2022-01-16 15:51:34 +0300

    file(diff): Add types

--- src/impl/diff.ts
+++ src/impl/diff.ts
@@ -1,34 +1,46 @@
-import Duration from "../duration.js";
+import Duration from "../duration";
+import type DateTime from "../datetime";
+import type { DurationOptions, PluralUnits } from "../types";
 
-function dayDiff(earlier, later) {
-  const utcDayStart = (dt) => dt.toUTC(0, { keepLocalTime: true }).startOf("day").valueOf(),
-    ms = utcDayStart(later) - utcDayStart(earlier);
+function dayDiff(earlier: DateTime, later: DateTime) {
+  const utcDayStart = (dt: DateTime) =>
+    dt.toUTC(0, { keepLocalTime: true }).startOf("day").valueOf();
+  const ms = utcDayStart(later) - utcDayStart(earlier);
   return Math.floor(Duration.fromMillis(ms).as("days"));
 }
 
-function highOrderDiffs(cursor, later, units) {
+function highOrderDiffs(cursor: DateTime, later: DateTime, units: PluralUnits[]) {
   const differs = [
-    ["years", (a, b) => b.year - a.year],
-    ["quarters", (a, b) => b.quarter - a.quarter],
-    ["months", (a, b) => b.month - a.month + (b.year - a.year) * 12],
+    ["years", (a: DateTime, b: DateTime) => b.year - a.year],
+    ["quarters", (a: DateTime, b: DateTime) => b.quarter - a.quarter],
+    ["months", (a: DateTime, b: DateTime) => b.month - a.month + (b.year - a.year) * 12],
     [
       "weeks",
-      (a, b) => {
+      (a: DateTime, b: DateTime) => {
         const days = dayDiff(a, b);
         return (days - (days % 7)) / 7;
       },
     ],
     ["days", dayDiff],
-  ];
+  ] as const;
 
-  const results = {};
-  let lowestOrder, highWater;
+  const results: { [index: string]: number } = {};
+  let lowestOrder: string | undefined;
+  let highWater: DateTime | undefined;
 
   for (const [unit, differ] of differs) {
     if (units.indexOf(unit) >= 0) {
+      /**
+       * Note: lowestOrder may be undefined, if units are empty array.
+       * In this case units.indexOf >= 0 will never be true.
+       */
       lowestOrder = unit;
 
       let delta = differ(cursor, later);
+      /**
+       * Note: highWater may be undefined, if units are empty array.
+       * In this case units.indexOf >= 0 will never be true.
+       */
       highWater = cursor.plus({ [unit]: delta });
 
       if (highWater > later) {
@@ -42,25 +54,33 @@ function highOrderDiffs(cursor, later, units) {
     }
   }
 
-  return [cursor, results, highWater, lowestOrder];
+  return { cursor, results, highWater, lowestOrder };
 }
 
-export default function (earlier, later, units, opts) {
-  let [cursor, results, highWater, lowestOrder] = highOrderDiffs(earlier, later, units);
+export default function (
+  earlier: DateTime,
+  later: DateTime,
+  units: PluralUnits[],
+  opts: DurationOptions
+) {
+  // eslint-disable-next-line prefer-const
+  let { cursor, results, highWater, lowestOrder } = highOrderDiffs(earlier, later, units);
 
-  const remainingMillis = later - cursor;
-
+  const remainingMillis = later.valueOf() - cursor.valueOf();
   const lowerOrderUnits = units.filter(
     (u) => ["hours", "minutes", "seconds", "milliseconds"].indexOf(u) >= 0
   );
 
   if (lowerOrderUnits.length === 0) {
-    if (highWater < later) {
-      highWater = cursor.plus({ [lowestOrder]: 1 });
+    if (highWater === undefined || highWater < later) {
+      highWater = lowestOrder !== undefined ? cursor.plus({ [lowestOrder]: 1 }) : cursor;
     }
 
     if (highWater !== cursor) {
-      results[lowestOrder] = (results[lowestOrder] || 0) + remainingMillis / (highWater - cursor);
+      if (lowestOrder !== undefined) {
+        results[lowestOrder] =
+          (results[lowestOrder] || 0) + remainingMillis / (highWater.valueOf() - cursor.valueOf());
+      }
     }
   }
 
