d74208e691bd15a7dbfd63a90f434d1f82733e92
Victor Malov <victor.malov@breeffy.com>
2022-01-18 14:52:44 +0300

    file(IANAZone): Add types, replace cache object to cache map, more strict checks

--- src/zones/IANAZone.ts
+++ src/zones/IANAZone.ts
@@ -1,12 +1,15 @@
-import { formatOffset, parseZoneInfo, isUndefined, ianaRegex, objToLocalTS } from "../impl/util.js";
-import Zone from "../zone.js";
+import { formatOffset, parseZoneInfo, isUndefined, objToLocalTS } from "../impl/util";
+import Zone from "../zone";
+import { LuxonError } from "../errors.js";
 
-const matchingRegex = RegExp(`^${ianaRegex.source}$`);
+import type { FormatOptions, FormatStyle } from "../types";
 
-let dtfCache = {};
-function makeDTF(zone) {
-  if (!dtfCache[zone]) {
-    dtfCache[zone] = new Intl.DateTimeFormat("en-US", {
+const dtfCache = new Map<string, Intl.DateTimeFormat>();
+
+function makeDTF(zone: string) {
+  const value = dtfCache.get(zone);
+  if (value === undefined) {
+    const dtf = new Intl.DateTimeFormat("en-US", {
       hour12: false,
       timeZone: zone,
       year: "numeric",
@@ -16,32 +19,63 @@ function makeDTF(zone) {
       minute: "2-digit",
       second: "2-digit",
     });
+    dtfCache.set(zone, dtf);
+    return dtf;
   }
-  return dtfCache[zone];
+  return value;
 }
 
-const typeToPos = {
-  year: 0,
-  month: 1,
-  day: 2,
-  hour: 3,
-  minute: 4,
-  second: 5,
-};
+const typeToPos = new Map<string, number>(
+  Object.entries({
+    year: 0,
+    month: 1,
+    day: 2,
+    hour: 3,
+    minute: 4,
+    second: 5,
+  })
+);
 
-function hackyOffset(dtf, date) {
-  const formatted = dtf.format(date).replace(/\u200E/g, ""),
-    parsed = /(\d+)\/(\d+)\/(\d+),? (\d+):(\d+):(\d+)/.exec(formatted),
-    [, fMonth, fDay, fYear, fHour, fMinute, fSecond] = parsed;
-  return [fYear, fMonth, fDay, fHour, fMinute, fSecond];
+function hackyOffset(dtf: Intl.DateTimeFormat, date: Date) {
+  const formatted = dtf.format(date).replace(/\u200E/g, "");
+  const parsed = /(\d+)\/(\d+)\/(\d+),? (\d+):(\d+):(\d+)/.exec(formatted);
+
+  if (parsed === null) {
+    // FIXME: Should we use custom error type?
+    throw new LuxonError(`Can't parse string "${formatted}" into parts`);
+  }
+
+  const [, fMonth, fDay, fYear, fHour, fMinute, fSecond] = parsed;
+  const result = [
+    Number(fYear),
+    Number(fMonth),
+    Number(fDay),
+    Number(fHour),
+    Number(fMinute),
+    Number(fSecond),
+  ];
+
+  result.forEach((it) => {
+    if (Number.isNaN(it)) {
+      /**
+       * Found invalid conversion from string to number, got NaN.
+       */
+      throw new LuxonError(
+        `Can't convert parts of string "${formatted}" into numbers, some part was converted to NaN`
+      );
+    }
+  });
+
+  return result;
 }
 
-function partsOffset(dtf, date) {
-  const formatted = dtf.formatToParts(date),
-    filled = [];
+function partsOffset(dtf: Intl.DateTimeFormat, date: Date) {
+  const formatted = dtf.formatToParts(date);
+  const filled: number[] = [];
+
   for (let i = 0; i < formatted.length; i++) {
-    const { type, value } = formatted[i],
-      pos = typeToPos[type];
+    const { type, value } = formatted[i];
+    const pos = typeToPos.get(type);
 
     if (!isUndefined(pos)) {
       filled[pos] = parseInt(value, 10);
@@ -50,21 +84,28 @@ function partsOffset(dtf, date) {
   return filled;
 }
 
-let ianaZoneCache = {};
+const ianaZoneCache = new Map<string, IANAZone>();
+
 /**
  * A zone identified by an IANA identifier, like America/New_York
  * @implements {Zone}
  */
 export default class IANAZone extends Zone {
+  zoneName: string;
+  valid: boolean;
+
   /**
    * @param {string} name - Zone name
    * @return {IANAZone}
    */
-  static create(name) {
-    if (!ianaZoneCache[name]) {
-      ianaZoneCache[name] = new IANAZone(name);
+  static create(name: string): IANAZone {
+    const value = ianaZoneCache.get(name);
+    if (value === undefined) {
+      const ianaZone = new IANAZone(name);
+      ianaZoneCache.set(name, ianaZone);
+      return ianaZone;
     }
-    return ianaZoneCache[name];
+    return value;
   }
 
   /**
@@ -72,8 +113,8 @@ export default class IANAZone extends Zone {
    * @return {void}
    */
   static resetCache() {
-    ianaZoneCache = {};
-    dtfCache = {};
+    ianaZoneCache.clear();
+    dtfCache.clear();
   }
 
   /**
@@ -84,7 +125,7 @@ export default class IANAZone extends Zone {
    * @deprecated This method returns false some valid IANA names. Use isValidZone instead
    * @return {boolean}
    */
-  static isValidSpecifier(s) {
+  static isValidSpecifier(s: string) {
     return this.isValidZone(s);
   }
 
@@ -96,8 +137,8 @@ export default class IANAZone extends Zone {
    * @example IANAZone.isValidZone("Sport~~blorp") //=> false
    * @return {boolean}
    */
-  static isValidZone(zone) {
-    if (!zone) {
+  static isValidZone(zone: string) {
+    if (zone.length === 0) {
       return false;
     }
     try {
@@ -108,7 +149,7 @@ export default class IANAZone extends Zone {
     }
   }
 
-  constructor(name) {
+  constructor(name: string) {
     super();
     /** @private **/
     this.zoneName = name;
@@ -132,25 +173,29 @@ export default class IANAZone extends Zone {
   }
 
   /** @override **/
-  offsetName(ts, { format, locale }) {
+  offsetName(ts: number, { format, locale }: FormatOptions) {
     return parseZoneInfo(ts, format, locale, this.name);
   }
 
   /** @override **/
-  formatOffset(ts, format) {
+  formatOffset(ts: number, format: FormatStyle) {
     return formatOffset(this.offset(ts), format);
   }
 
   /** @override **/
-  offset(ts) {
+  offset(ts: number) {
     const date = new Date(ts);
 
-    if (isNaN(date)) return NaN;
+    if (isNaN(date.valueOf())) {
+      return NaN;
+    }
 
-    const dtf = makeDTF(this.name),
-      [year, month, day, hour, minute, second] = dtf.formatToParts
-        ? partsOffset(dtf, date)
-        : hackyOffset(dtf, date);
+    const dtf = makeDTF(this.name);
+
+    // @ts-expect-error Do we check here formatToParts method existence?
+    const [year, month, day, hour, minute, second] = dtf.formatToParts
+      ? partsOffset(dtf, date)
+      : hackyOffset(dtf, date);
 
     // because we're using hour12 and https://bugs.chromium.org/p/chromium/issues/detail?id=1025564&can=2&q=%2224%3A00%22%20datetimeformat
     const adjustedHour = hour === 24 ? 0 : hour;
@@ -165,14 +210,14 @@ export default class IANAZone extends Zone {
       millisecond: 0,
     });
 
-    let asTS = +date;
+    let asTS = date.valueOf();
     const over = asTS % 1000;
     asTS -= over >= 0 ? over : 1000 + over;
     return (asUTC - asTS) / (60 * 1000);
   }
 
   /** @override **/
-  equals(otherZone) {
+  equals(otherZone: Zone) {
     return otherZone.type === "iana" && otherZone.name === this.name;
   }
 
