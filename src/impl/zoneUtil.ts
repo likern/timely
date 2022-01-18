3b165fc84ba690426ba93ec033f64010e6445dcc
Victor Malov <victor.malov@breeffy.com>
2022-01-18 17:11:26 +0300

    file(zoneUtil): fix imports, add types

--- src/impl/zoneUtil.ts
+++ src/impl/zoneUtil.ts
@@ -2,15 +2,17 @@
  * @private
  */
 
-import Zone from "../zone.js";
-import IANAZone from "../zones/IANAZone.js";
-import FixedOffsetZone from "../zones/fixedOffsetZone.js";
-import InvalidZone from "../zones/invalidZone.js";
+import Zone from "../zone";
+import IANAZone from "../zones/IANAZone";
+import FixedOffsetZone from "../zones/fixedOffsetZone";
+import InvalidZone from "../zones/invalidZone";
 
-import { isUndefined, isString, isNumber } from "./util.js";
+import { isUndefined, isString, isNumber } from "./util";
 
-export function normalizeZone(input, defaultZone) {
-  let offset;
+export function normalizeZone(
+  input: string | number | Zone | null | undefined,
+  defaultZone: string
+): string | Zone {
   if (isUndefined(input) || input === null) {
     return defaultZone;
   } else if (input instanceof Zone) {
@@ -22,10 +24,6 @@ export function normalizeZone(input, defaultZone) {
     else return FixedOffsetZone.parseSpecifier(lowered) || IANAZone.create(input);
   } else if (isNumber(input)) {
     return FixedOffsetZone.instance(input);
-  } else if (typeof input === "object" && input.offset && typeof input.offset === "number") {
-    // This is dumb, but the instanceof check above doesn't seem to really work
-    // so we're duck checking it
-    return input;
   } else {
     return new InvalidZone(input);
   }
